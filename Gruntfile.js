1ebba5f4fd671d425137cd69a4e969a4e882edd6
Marian Friedmann <marian.friedmann@gmail.com>
2014-02-15 15:54:27 +0100

    gruntconf & tests setup

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        dirs: {
+            basicss: 'basicss',
+            css: 'tests/assets/css'
+        },
+
+        // SCSS
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    '<%= dirs.css %>/style.css': '<%= dirs.css %>/style.scss'
+                }
+            },
+            build: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    '<%= dirs.css %>/style.css': '<%= dirs.css %>/style.scss'
+                }
+            }
+        },
+
+        // CSS autoprefixer
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions']
+            },
+            dist: {
+                files: {
+                    '<%= dirs.css %>/style.css': '<%= dirs.css %>/style.css'
+                }
+            }
+        },
+
+        // Connect Server
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    base: 'tests'
+                }
+            }
+        },
+
+        // Watch
+        watch: {
+            options: {
+                livereload: true
+            },
+            sass: {
+                files: [
+                    '<%= dirs.css %>/*.scss',
+                    '*.scss',
+                    'basics/**/*.scss'
+                ],
+                tasks: ['sass:dev', 'autoprefixer'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['sass:build', 'autoprefixer']);
+    grunt.registerTask('dev', ['connect', 'watch']);
+};
